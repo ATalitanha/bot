e0f1e9d521ceec61fb33a8e4739cd73e16202490
AliTanha <tmt_ali@yahoo.com>
2025-08-01 20:21:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
 from fastapi import FastAPI, Request
 from telegram import Update
-from telegram.ext import ApplicationBuilder
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.environ["BOT_TOKEN"]
 
 app = FastAPI()
 bot_app = ApplicationBuilder().token(TOKEN).build()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! ربات Webhook فعال و آماده است.")
+
+bot_app.add_handler(CommandHandler("start", start))
 
 @app.post("/webhook")
 async def handle_webhook(req: Request):
