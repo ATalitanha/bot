4cfbcbd19c42afe9614f6220d90632a1d2df7ead
AliTanha <tmt_ali@yahoo.com>
2025-08-01 19:22:30 +0330

    create

---
+++ main.py
@@ -0,0 +1,20 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from fastapi import FastAPI
+import uvicorn
+import os
+
+TOKEN = os.environ["8383278819:AAG9H3noSAgKNYXtcxp4vO0ZIRNTPvrnZFY"]
+
+app = FastAPI()
+
+telegram_app = ApplicationBuilder().token(TOKEN).build()
+
+@app.get("/")
+async def root():
+    return {"status": "Bot is running"}
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! ربات فعاله و روی Render هست!")
+
+telegram_app.add_handler(CommandHandler("start", start))
