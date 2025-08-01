12463d3e733223dbd7a3150915208a60fe5563fa
AliTanha <tmt_ali@yahoo.com>
2025-08-01 19:38:07 +0330

    fix

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from fastapi import FastAPI
-import uvicorn
 import os
 
 TOKEN = os.environ["BOT_TOKEN"]
 
-app = FastAPI()
-
-telegram_app = ApplicationBuilder().token(TOKEN).build()
-
-@app.get("/")
-async def root():
-    return {"status": "Bot is running"}
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! ربات فعاله و روی Render هست!")
+    await update.message.reply_text("سلام! ربات پایتونی من فعاله!")
 
-telegram_app.add_handler(CommandHandler("start", start))
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
