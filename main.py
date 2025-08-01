7f9936b8646fe90de801e3a134a163adec601e53
AliTanha <tmt_ali@yahoo.com>
2025-08-01 19:54:40 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.environ["BOT_TOKEN"]
+TOKEN = os.environ["8383278819:AAG9H3noSAgKNYXtcxp4vO0ZIRNTPvrnZFY"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! ربات پایتونی من فعاله!")
